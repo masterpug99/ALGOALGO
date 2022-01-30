82dcbfe570558832ce4b0997d5e57163a60471b9
masterpug99 <masterpug99@gmail.com>
2022-01-30 14:56:16 +0900

    .

--- ALGOALGO/[BOJ]11650.cpp
+++ ALGOALGO/[BOJ]11650.cpp
@@ -2,13 +2,13 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-
+/*
 bool cmp(pair<int, int>a, pair<int, int>b) {
 	if (a.first > b.first) return false;
 	else if (a.first == b.first) return a.second < b.second;
 	else return true;
 }
-/*
+
 int main() {
 	int N;
 	cin >> N;
