15256e8b702812b22ac313091b33599d1a8b9def
masterpug99 <masterpug99@gmail.com>
2022-02-05 11:45:40 +0900

    2022-02-05(11:45)

---
+++ ALGOALGO/[BOJ]11279.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+/*
+int main() {
+	priority_queue<int> q;
+	int n;
+	cin >> n;
+	while (n--) {
+		int number;
+		cin >> number;
+		if (number == 0) {
+			if (!q.empty()) {
+				cout << q.top() << "\n";
+				q.pop();
+			}
+			else {
+				cout << 0 << "\n";
+			}
+		}
+		else {
+			q.push(number);
+		}
+
+	}
+}
+*/
---
+++ ALGOALGO/[BOJ]9095.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+/*
+int ans = 0;
+void DFS(int number,int t,int cnt) {
+	if (t == number) {
+		ans++;
+	}
+	if (t > number)return;
+	DFS(number, t + 1,cnt+1);
+	DFS(number, t + 2,cnt+1);
+	DFS(number, t + 3,cnt+1);
+}
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int number;
+		cin >> number;
+		ans = 0;
+		DFS(number,0,0);
+		cout << ans << "\n";
+	}
+}
+*/
