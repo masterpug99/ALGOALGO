ac9bf80b14bf284aac964b92465ea1498cd31258
masterpug99@gmail.com <masterpug99@gmail.com>
2022-03-06 15:39:36 +0900

    2022-03-06(15:39)

---
+++ ALGOALGO/[BOJ]17219.cpp
@@ -0,0 +1,24 @@
+#include <map>
+#include <iostream>
+#include <string>
+using namespace std;
+/*
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n, m;
+	cin >> n >> m;
+	map<string, string> map;
+	for (int i = 0; i < n; i++) {
+		string a, b;
+		cin >> a >> b;
+		map[a] = b;
+	}
+	for (int i = 0; i < m; i++) {
+		string str;
+		cin >> str;
+		cout << map[str] << "\n";
+	}
+}
+*/
---
+++ ALGOALGO/[BOJ]17626.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int list[224];
+int dp[50001];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	int k = 0;
+	for (int i = 0; i <= 50000; i++) {
+		dp[i] = 10;
+	}
+	dp[0] = 0;
+	for (int i = 1; i <= 223; i++) {
+		list[i] = i * i;
+		if (list[i] <= n) {
+			k = i;
+		}
+		dp[list[i]] = 1;
+	}
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= k; j++) {
+			if (i >= list[j]) {
+				if ((dp[i - list[j]] + dp[list[j]]) <= 4) {
+					dp[i] = min(dp[i], dp[i - list[j]] + dp[list[j]]);
+				}
+			}
+		}
+	}
+	cout << dp[n];
+}
